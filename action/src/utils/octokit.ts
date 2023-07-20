129c034895f7319d30d17663af3ecddd1d2f8d3b
Daniel Stoyanoff <dannystoyanoff@gmail.com>
2023-07-20 14:58:40 +0300

    bring back node-fetch

--- action/src/utils/octokit.ts
+++ action/src/utils/octokit.ts
@@ -1,7 +1,11 @@
 import { getToken } from "./inputs";
 import { Octokit } from "@octokit/rest";
+import fetch from "node-fetch";
 
 export const getOctokit = () =>
   new Octokit({
     auth: getToken(),
+    request: {
+      fetch,
+    },
   });
