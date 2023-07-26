5ea2b84055f3b8fcd478f2eeab6c5fcbf844a665
Daniel Stoyanoff <dannystoyanoff@gmail.com>
2023-07-26 18:36:04 +0300

    debug parents

--- action/src/update-release.ts
+++ action/src/update-release.ts
@@ -10,38 +10,15 @@ import { createCommit, getCommit } from "./github-api/commit";
 import { createPullRequest, getPullRequestsByCommit } from "./github-api/pr";
 import { extractBranchFromRef } from "./utils/ref";
 
-export const updateRelease = async () => {
+export async function updateRelease() {
   const commit = await getCommit(github.context.sha);
-  const prs = await getPullRequestsByCommit(github.context.sha);
-
-  const pr = prs.find(
-    (pr) => pr.base.ref === extractBranchFromRef(github.context.ref)
-  );
-
-  if (!pr) {
-    throw new Error(`Could not find PR for ref ${github.context.ref}`);
-  }
+  const pr = await getPr();
 
   const choreBranchName = `chore/hotfix-merge-${pr.number}`;
-
-  const devBranch = await getBranch("dev");
-
-  if (!devBranch) {
-    throw new Error("Dev branch not resolved");
-  }
-
-  // when creating the release PR, the push gets triggered,
-  // but at that point we should not create a dev PR as there is no commit difference
-  if (devBranch.commit.sha === commit.sha) {
-    core.info("No diff to dev, so no PR will be opened");
-    return;
-  }
-
-  await createBranch(choreBranchName, devBranch.commit.sha);
-  const newBranch = await getBranch(choreBranchName);
+  const newBranch = await createChoreBranch(choreBranchName, commit.sha);
 
   if (!newBranch) {
-    throw new Error(`Branch ${choreBranchName} not resolved`);
+    return;
   }
 
   const branchSha = newBranch.commit.sha;
@@ -50,6 +27,7 @@ export const updateRelease = async () => {
     `Created branch ${choreBranchName} (sha: ${branchSha}, tree: ${branchTree})`
   );
 
+  console.dir(commit.parents, { depth: null });
   const parentSha = commit.parents[0].sha;
   const tempCommit = await createCommit(branchTree, parentSha, "temp");
 
@@ -70,4 +48,36 @@ export const updateRelease = async () => {
     `chore(release): Merge release fix ${pr.number} to dev`,
     pr.body ?? commit.message
   );
-};
+}
+
+async function getPr() {
+  const prs = await getPullRequestsByCommit(github.context.sha);
+
+  const pr = prs.find(
+    (pr) => pr.base.ref === extractBranchFromRef(github.context.ref)
+  );
+
+  if (!pr) {
+    throw new Error(`Could not find PR for ref ${github.context.ref}`);
+  }
+
+  return pr;
+}
+
+async function createChoreBranch(name: string, fixCommitSha: string) {
+  const devBranch = await getBranch("dev");
+
+  if (!devBranch) {
+    throw new Error("Dev branch not resolved");
+  }
+
+  // when creating the release PR, the push gets triggered,
+  // but at that point we should not create a dev PR as there is no commit difference
+  if (devBranch.commit.sha === fixCommitSha) {
+    core.info("No diff to dev, so no PR will be opened");
+    return null;
+  }
+
+  await createBranch(name, devBranch.commit.sha);
+  return getBranch(name);
+}
